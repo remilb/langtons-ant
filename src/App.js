5d75890a7b5790cd389676f15e2dcaf49d43c39e
Ryan Bauer <ryanemilbauer@gmail.com>
2019-03-24 22:09:34 -0700

    Slider for animation speed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Slider from "@material-ui/lab/Slider";
+
 import LangtonsAnt from "./LangtonsAnt";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <LangtonsAnt
-          gridWidth={500}
-          gridHeight={500}
-          squareWidth={10}
-          animInterval={3000}
-        />
-      </div>
-    );
-  }
+function App(props) {
+  const [animSpeed, setAnimSpeed] = useState(500);
+
+  return (
+    <div className="App">
+      <LangtonsAnt
+        gridWidth={500}
+        gridHeight={500}
+        squareWidth={10}
+        animInterval={animSpeed}
+      />
+      <Slider
+        width={500}
+        value={animSpeed}
+        min={1}
+        max={1000}
+        onChange={(e, value) => setAnimSpeed(value)}
+      />
+    </div>
+  );
 }
 
 export default App;
