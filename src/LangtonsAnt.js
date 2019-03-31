734d05b902cc707e1ec1e053b5685369f6115fab
Ryan Bauer <ryanemilbauer@gmail.com>
2019-03-30 23:27:25 -0700

    Refactoring in anticipation of custom rules feature

--- src/LangtonsAnt.js
+++ src/LangtonsAnt.js
@@ -1,8 +1,12 @@
 import React, { useState, useRef, useEffect, useCallback } from "react";
+import { useInterval } from "./utils";
 
 function LangtonsAnt(props) {
   const {
-    rules,
+    rules = {
+      white: { nextColor: "black", rotation: "l", numSteps: 1 },
+      black: { nextColor: "white", rotation: "r", numSteps: 1 }
+    },
     gridWidth,
     gridHeight,
     squareWidth,
@@ -28,46 +32,44 @@ function LangtonsAnt(props) {
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-
     let gridState = {};
-    function getCurColor(curPos) {
-      if (gridState[curPos] == undefined) {
-        return "white";
-      }
-      return gridState[curPos]; // ? gridHeight[curPos] : "white";
-    }
-
     let newPos = [0, 0];
     let newDir = [-1, 0];
     let newColor = "white";
+
     for (let i = 0; i < prerenderSteps; i++) {
       let oldPos = newPos.slice(0);
-      ({ newPos, newDir, newColor } = takeStepV2(newPos, newDir, getCurColor));
+      ({ newPos, newDir, newColor } = takeStep(
+        newPos,
+        newDir,
+        gridState[newPos] ? gridState[newPos] : "white",
+        rules
+      ));
       gridState[oldPos] = newColor;
     }
 
-    for (let pos in gridState) {
-      const [x, y] = pos.split(",");
-      const [canvasX, canvasY] = [
-        canvas.width / 2 + Number(x) * squareWidth,
-        canvas.height / 2 + Number(y) * -squareWidth
-      ];
-      context.fillStyle = gridState[pos];
-      context.fillRect(canvasX, canvasY, squareWidth, squareWidth);
-    }
+    drawCells(canvas, gridState, squareWidth);
 
     antState.current = { pos: newPos, dir: newDir };
   }, [prerenderSteps]);
 
   useInterval(
     () => {
-      const { newPos, newDir } = takeStep(
-        canvasRef.current,
-        antState.current.pos,
-        antState.current.dir,
-        squareWidth
+      const canvas = canvasRef.current;
+      let curPos = antState.current.pos;
+      let curDir = antState.current.dir;
+      let curColor = getCellColorFromCanvas(canvas, curPos, squareWidth);
+
+      const { newPos, newDir, newColor } = takeStep(
+        curPos,
+        curDir,
+        curColor,
+        rules
       );
+
+      let cellData = {};
+      cellData[curPos] = newColor;
+      drawCells(canvas, cellData, squareWidth);
 
       antState.current = { pos: newPos, dir: newDir };
     },
@@ -77,77 +79,61 @@ function LangtonsAnt(props) {
   return <canvas ref={canvasRef} width={gridWidth} height={gridHeight} />;
 }
 
-function takeStepV2(curPos, curDir, getCurColor, rules) {
-  let curColor = getCurColor(curPos);
-  let newColor, newPos, newDir;
-  //Execute rules based on current color
-  //let {newPos, newDir, newColor} = executeRules()
-  if (curColor === "white") {
-    newColor = "black";
-    newDir = [curDir[1], -curDir[0]];
-  } else {
-    newColor = "white";
-    newDir = [-curDir[1], curDir[0]];
+function drawCells(canvas, cellData, cellWidth) {
+  const context = canvas.getContext("2d");
+  for (let pos in cellData) {
+    const [x, y] = pos.split(",");
+    const [canvasX, canvasY] = [
+      canvas.width / 2 + Number(x) * cellWidth,
+      canvas.height / 2 + Number(y) * -cellWidth
+    ];
+    context.fillStyle = cellData[pos];
+    context.fillRect(canvasX, canvasY, cellWidth, cellWidth);
   }
-  newPos = [curPos[0] + newDir[0], curPos[1] + newDir[1]];
+}
+
+function getCellColorFromCanvas(canvas, pos, cellWidth) {
+  const context = canvas.getContext("2d");
+  const [canvasX, canvasY] = [
+    canvas.width / 2 + pos[0] * cellWidth,
+    canvas.height / 2 + pos[1] * -cellWidth
+  ];
+  const curSquare = context.getImageData(canvasX, canvasY, 1, 1);
+  const [r, g, b] = curSquare.data;
+  return r === 255 && g === 255 && b === 255 ? "white" : "black";
+}
+
+// function projectCoordinatesToCanvas()
+
+function takeStep(curPos, curDir, curColor, rules) {
+  //let curColor = curColor(curPos);
+  let { newPos, newDir, newColor } = executeRules(
+    rules,
+    curColor,
+    curPos,
+    curDir
+  );
 
   return { newPos, newDir, newColor };
 }
 
-function takeStep(canvas, curPos, curDir, squareWidth) {
-  let newPos = [0, 0];
-  let newDir = [0, 0];
-  const ctx = canvas.getContext("2d");
-  const [antCanvasX, antCanvasY] = [
-    canvas.width / 2 + curPos[0] * squareWidth,
-    canvas.height / 2 + curPos[1] * -squareWidth
-  ];
-
-  ctx.save();
-  ctx.translate(antCanvasX, antCanvasY);
-
-  // Not affected by transformation matrix
-  const curSquare = ctx.getImageData(antCanvasX, antCanvasY, 1, 1);
-  const [r, g, b] = curSquare.data;
-  if (r === 255 && g === 255 && b === 255) {
-    ctx.fillStyle = "black";
-    newDir = [curDir[1], -curDir[0]];
-  } else {
-    ctx.fillStyle = "white";
-    newDir = [-curDir[1], curDir[0]];
-  }
-  newPos = [curPos[0] + newDir[0], curPos[1] + newDir[1]];
-
-  ctx.fillRect(0, 0, squareWidth, squareWidth);
-  ctx.restore();
-
-  return { newPos, newDir };
-}
-
 function executeRules(rules, curColor, curPos, curDir) {
-  // return a new direction and position
-  return;
+  let rule = rules[curColor];
+  let newColor = rule.nextColor;
+  let newDir = applyRotation(rule.rotation, curDir);
+  let newPos = curPos.map((e, i) => e + newDir[i] * rule.numSteps);
+  return { newPos, newDir, newColor };
 }
 
-// Courtesy of Dan Abramov
-function useInterval(callback, delay) {
-  const savedCallback = useRef();
-
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
+function applyRotation(rot, dir) {
+  switch (rot) {
+    case "r":
+      return [dir[1], -dir[0]];
+    case "l":
+      return [-dir[1], dir[0]];
+    default:
+      throw new Error("Invalid rotation supplied in rule set");
+  }
 }
 
 export default LangtonsAnt;
