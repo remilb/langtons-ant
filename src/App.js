6225f9c0fbf33d355dbc3ead28f4e4343958513c
Ryan Bauer <ryanemilbauer@gmail.com>
2019-05-10 23:07:08 -0700

    Update app layout using new components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
-import "./App.css";
 
-import { Grid, Drawer, Typography, Paper } from "@material-ui/core";
+import {
+  Grid,
+  Drawer,
+  Typography,
+  Paper,
+  CssBaseline
+} from "@material-ui/core";
 
 import AppHeader from "./components/AppHeader";
+import PlaybackControls from "./components/PlaybackControls";
 import LangtonsAntCanvas from "./components/LangtonsAntCanvas";
-import Controls from "./Controls";
+import SetttingsDrawer from "./components/SettingsDrawer";
+import RulesDrawer from "./components/RulesDrawer";
 import Rules from "./Rules";
 
+import RuleItem from "./components/RuleItem";
+
 function App(props) {
+  const [showSettings, setShowSettings] = useState(false);
+  const [settings, setSettings] = useState({
+    prerenderSteps: 0,
+    gridType: "square"
+  });
+
   const [animSpeed, setAnimSpeed] = useState(30);
-  const [prerenderSteps, setPrerenderSteps] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isResetting, setIsResetting] = useState(true);
+
+  const [showRules, setShowRules] = useState(true);
   const [rules, setRules] = useState([
     { onColor: "#ffffff", nextColor: "#000000", rotation: "l", numSteps: 1 },
     { onColor: "#000000", nextColor: "#37d67a", rotation: "r", numSteps: 1 },
@@ -21,46 +37,59 @@ function App(props) {
   ]);
 
   return (
-    <div className="App">
-      <AppHeader />
-      <Grid container alignItems="center" justify="center" spacing={16}>
-        <Drawer anchor="left" open={true} variant="persistent" elevation={16}>
-          <Controls
-            onPlayPause={(e, v) => setIsPlaying(!isPlaying)}
-            isPlaying={isPlaying}
-            minAnimInterval={20}
-            maxAnimInterval={60}
-            animInterval={animSpeed}
-            onAnimIntervalUpdate={(e, v) => setAnimSpeed(v)}
-            prerenderSteps={prerenderSteps}
-            onPrerenderStepsChange={(e, v) => setPrerenderSteps(v)}
-            handleReset={() => setIsResetting(true)}
-          />
-        </Drawer>
-        <Grid item>
-          <LangtonsAntCanvas
-            rules={rulesArrayToMap(rules)}
-            cellType="hex"
-            cellSize={3}
-            canvasWidth={window.innerWidth}
-            canvasHeight={window.innerHeight}
-            prerenderSteps={prerenderSteps}
-            animInterval={animSpeed}
-            isAnimating={isPlaying}
-            isResetting={isResetting}
-            onResetComplete={() => setIsResetting(false)}
-          />
-        </Grid>
-      </Grid>
-      <Drawer anchor="right" open={true} variant="persistent" elevation={16}>
+    <>
+      <CssBaseline />
+      <AppHeader
+        handleClickSettings={() => setShowSettings(!showSettings)}
+        handleClickRules={() => setShowRules(!showRules)}
+      />
+      <SetttingsDrawer
+        settings={settings}
+        handleSettingsChange={setSettings}
+        open={showSettings}
+        onClose={() => setShowSettings(false)}
+      />
+      <LangtonsAntCanvas
+        rules={rulesArrayToMap(rules)}
+        cellType={settings.gridType}
+        cellSize={3}
+        canvasWidth={document.documentElement.clientWidth}
+        canvasHeight={document.documentElement.clientHeight}
+        prerenderSteps={settings.prerenderSteps}
+        animInterval={animSpeed}
+        isAnimating={isPlaying}
+        isResetting={isResetting}
+        onResetComplete={() => setIsResetting(false)}
+      />
+      <RulesDrawer
+        open={showRules}
+        rules={rules}
+        onRulesChange={newRules => setRules(newRules)}
+        onClose={() => setShowRules(false)}
+      />
+      <Drawer anchor="bottom" open={false} variant="persistent" elevation={16}>
         <Rules rules={rules} onRulesChange={newRules => setRules(newRules)} />
       </Drawer>
-    </div>
+      <PlaybackControls
+        onPlayPause={(e, v) => setIsPlaying(!isPlaying)}
+        isPlaying={isPlaying}
+        minAnimInterval={20}
+        maxAnimInterval={60}
+        animInterval={animSpeed}
+        onAnimIntervalUpdate={(e, v) => setAnimSpeed(v)}
+        handleReset={() => setIsResetting(true)}
+      />
+    </>
   );
 }
 
 function rulesArrayToMap(rulesArray) {
-  return Object.fromEntries(rulesArray.map(rule => [rule.onColor, rule]));
+  let rulesMap = {};
+  for (let rule of rulesArray) {
+    const { onColor, ...rest } = rule;
+    rulesMap[onColor] = rest;
+  }
+  return rulesMap;
 }
 
 export default App;
