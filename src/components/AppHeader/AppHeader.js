b28bdb339403b58777b753bb5a0ecf1de7992919
Ryan Bauer <ryanemilbauer@gmail.com>
2019-05-10 23:06:30 -0700

    Add buttons for Rules and Settings to AppHeader

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import SettingsIcon from "@material-ui/icons/Settings";
 
-function AppHeader() {
+const useStyles = makeStyles(theme => ({
+  title: {
+    textAlign: "left",
+    flexGrow: 1
+  }
+}));
+
+function AppHeader(props) {
+  const classes = useStyles();
   return (
     <AppBar position="fixed">
       <Toolbar>
-        <Typography variant="h6" color="inherit">
+        <IconButton onClick={props.handleClickRules} aria-label="Open rules">
+          <SettingsIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" className={classes.title}>
           Langton's Ant
         </Typography>
+        <IconButton
+          onClick={props.handleClickSettings}
+          aria-label="Open settings"
+        >
+          <SettingsIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
