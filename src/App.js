c50ca1d28fd8b51b4cc6d963face71e97a8a87d5
Ryan Bauer <ryanemilbauer@gmail.com>
2019-05-28 03:50:54 -0700

    More fun default rules. Bored of looking at the classic

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ function App() {
   const [showSettings, setShowSettings] = useState(false);
   const [settings, setSettings] = useState({
     prerenderSteps: 0,
-    gridType: "square"
+    gridType: "hex"
   });
 
   const [animSpeed, setAnimSpeed] = useState(32);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isResetting, setIsResetting] = useState(true);
 
-  const [cellSize, setCellSize] = useState(2);
+  const [cellSize, setCellSize] = useState(3);
   // Can make this more sophisticated later
   const handleWheel = e => {
     if (e.deltaY < 0) {
@@ -32,10 +32,18 @@ function App() {
 
   const [showRules, setShowRules] = useState(true);
   const [rules, setRules] = useState([
-    { onColor: "#ffffff", nextColor: "#000000", rotation: "l", numSteps: 1 },
+    { onColor: "#ffffff", nextColor: "#000000", rotation: "r", numSteps: 1 },
     { onColor: "#000000", nextColor: "#37d67a", rotation: "r", numSteps: 1 },
-    { onColor: "#37d67a", nextColor: "#ff8a65", rotation: "r", numSteps: 1 },
-    { onColor: "#ff8a65", nextColor: "#ffffff", rotation: "l", numSteps: 1 }
+    { onColor: "#37d67a", nextColor: "#ff8a65", rotation: "l", numSteps: 1 },
+    { onColor: "#ff8a65", nextColor: "#1f28cd", rotation: "l", numSteps: 1 },
+    { onColor: "#1f28cd", nextColor: "#f59e06", rotation: "l", numSteps: 1 },
+    { onColor: "#f59e06", nextColor: "#436bc3", rotation: "r", numSteps: 1 },
+    { onColor: "#436bc3", nextColor: "#9f6773", rotation: "l", numSteps: 1 },
+    { onColor: "#9f6773", nextColor: "#dce775", rotation: "l", numSteps: 1 },
+    { onColor: "#dce775", nextColor: "#f6e76b", rotation: "l", numSteps: 1 },
+    { onColor: "#f6e76b", nextColor: "#ca76c8", rotation: "r", numSteps: 1 },
+    { onColor: "#ca76c8", nextColor: "#53b4b7", rotation: "r", numSteps: 1 },
+    { onColor: "#53b4b7", nextColor: "#ffffff", rotation: "r", numSteps: 1 }
   ]);
 
   const windowSize = useDebounce(useWindowSize(), 250);
