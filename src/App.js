689f49d244941b2c7d8ddb5a2918cbe20cea7e6e
Ryan Bauer <ryanemilbauer@gmail.com>
2019-07-12 16:44:57 -0700

    Change default ruleset to big spiral to ease panning development

--- src/App.js
+++ src/App.js
@@ -22,18 +22,13 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [showRules, setShowRules] = useState(true);
   const [rules, setRules] = useState([
-    { onColor: "#ffffff", nextColor: "#000000", rotation: "r", numSteps: 1 },
-    { onColor: "#000000", nextColor: "#37d67a", rotation: "r", numSteps: 1 },
-    { onColor: "#37d67a", nextColor: "#ff8a65", rotation: "l", numSteps: 1 },
-    { onColor: "#ff8a65", nextColor: "#1f28cd", rotation: "l", numSteps: 1 },
-    { onColor: "#1f28cd", nextColor: "#f59e06", rotation: "l", numSteps: 1 },
-    { onColor: "#f59e06", nextColor: "#436bc3", rotation: "r", numSteps: 1 },
-    { onColor: "#436bc3", nextColor: "#9f6773", rotation: "l", numSteps: 1 },
-    { onColor: "#9f6773", nextColor: "#dce775", rotation: "l", numSteps: 1 },
-    { onColor: "#dce775", nextColor: "#f6e76b", rotation: "l", numSteps: 1 },
-    { onColor: "#f6e76b", nextColor: "#ca76c8", rotation: "r", numSteps: 1 },
-    { onColor: "#ca76c8", nextColor: "#53b4b7", rotation: "r", numSteps: 1 },
-    { onColor: "#53b4b7", nextColor: "#ffffff", rotation: "r", numSteps: 1 }
+    { onColor: "#ffffff", nextColor: "#000000", rotation: "l", numSteps: 1 },
+    { onColor: "#000000", nextColor: "#37d67a", rotation: "l2", numSteps: 1 },
+    { onColor: "#37d67a", nextColor: "#ff8a65", rotation: "n", numSteps: 1 },
+    { onColor: "#ff8a65", nextColor: "#1f28cd", rotation: "u", numSteps: 1 },
+    { onColor: "#1f28cd", nextColor: "#f59e06", rotation: "l2", numSteps: 1 },
+    { onColor: "#f59e06", nextColor: "#436bc3", rotation: "l", numSteps: 1 },
+    { onColor: "#436bc3", nextColor: "#ffffff", rotation: "r2", numSteps: 1 }
   ]);
 
   // Handlers
